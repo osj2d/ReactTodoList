a7a96067083bd07356e744c2f1b3b160f2744c56
osj2d <juniorsavi97@gmail.com>
2024-01-13 10:22:30 -0300

    ajustado

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -4,8 +4,7 @@ import styles from "./item.module.css";
 const Item = ({  id, task, complete }) => {
   return (
     <div >
-      <h2>{id}</h2>
-      <p className={complete ? styles.completa : styles.completa1}>{task}</p>
+      <p className={`${styles.task} ${complete ? styles.completa : styles.completa1}`}>{task}</p>
     </div>
   );
 };
